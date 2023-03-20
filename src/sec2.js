88f099d9f0ca29f25aa4e0ca3f5cd8f04dcadd87
SaraSaif119 <sarasaifsaif9@gmail.com>
2023-03-20 15:37:25 +0200

    first commit

--- src/sec2.js
+++ src/sec2.js
@@ -1,11 +1,11 @@
 import React from 'react';
 function Sec2() {
     return (
-        <div class="card col-md-12 ">
-            <div class="row div6">
-            <div class="col-md-6">
+        <div class="row card div6">
+           
+            <div class="col-lg-8 col-md-8 col-sm-12">
             </div>
-            <div class="col-md-6">
+            <div class=" col-md-12">
             <div class="row card-body btn2" >
                 <h1 class="card-text text-light">GET IN TOUCH </h1>
                 <input placeholder="FIRST NAME" class="inp"/>
@@ -15,7 +15,7 @@ function Sec2() {
                 <input placeholder="MESSAGE" class="inp2"/>
                 <button  class="inp3">SEND</button>
             </div>
-            </div>
+       
             </div>
         </div>
     );
